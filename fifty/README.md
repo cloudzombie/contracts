0633e6126d74d532408c836c2833433b62f68d94
Xavier Yvez <xavieryvez@gmail.com>
2016-02-14 19:16:46 +0200

    initial commit, untested coding of idea

---
+++ fifty/README.md
@@ -0,0 +1,21 @@
+# fifty
+
+Ethereum contract for the [Looney Fifty](http://the.looney.farm/game/lottery)
+
+## randomness
+
+The approach is adpated from what is employed in the [lottery](../lottery/README.md), so the reader should be familiar with that.
+
+Two Lehmer generators are used, the first running for each transaction received combining the long-running result with a mix from the coinbase and blockhash, the second for executes for each play that occurs within the transaction, adapting the overall running result chain.
+
+```
+  seeda = (seeda * LEHMER_MUL) % LEHMER_MOD;
+  result = result ^ uint(sha3(block.coinbase, block.blockhash(block.number - 1), pool, seeda));
+  ...
+  for (uint num = 0; num < number; num++) {
+    seedb = (seedb * LEHMER_MUL) % LEHMER_MOD;
+    result = result ^ seedb;
+
+    if (result % 2 == 0) {
+      ...
+```
---
+++ fifty/fifty.sol
@@ -0,0 +1,98 @@
+contract LooneyFifty {
+  modifier owneronly {
+    if (msg.sender != owner) {
+      throw;
+    }
+
+    _
+  }
+
+  modifier pricecheck {
+    if (msg.value < CONFIG_MIN_VALUE) {
+      throw;
+    }
+
+    _
+  }
+
+  event NewPlay(address addr, uint32 at, uint input, uint output, uint pool);
+
+  uint constant private LEHMER_MOD = 4294967291;
+  uint constant private LEHMER_MUL = 279470273;
+  uint constant private LEHMER_SDA = 1299709;
+  uint constant private LEHMER_SDB = 7919;
+
+  uint constant public CONFIG_MAX_PLAYS = 100;
+  uint constant public CONFIG_PRICE = 10 finney;
+  uint constant public CONFIG_MIN_VALUE = CONFIG_PRICE;
+  uint constant public CONFIG_MAX_VALUE = CONFIG_PRICE * CONFIG_MAX_PLAYS;
+  uint constant public CONFIG_FEES_MUL = 5;
+  uint constant public CONFIG_FEES_DIV = 1000;
+  uint constant public CONFIG_RATIO_PRES = 5;
+  uint constant public CONFIG_RATIO_MUL = 10 ** CONFIG_RATIO_PRES;
+
+  address private owner = msg.sender;
+  uint private pool = msg.value;
+  uint private fees = 0;
+
+  uint private result = uint(sha3(block.coinbase, block.blockhash(block.number - 1), pool, now));
+  uint private seeda = LEHMER_SDA;
+  uint private seedb = LEHMER_SDB;
+
+  uint public txs = 0;
+
+  function LooneyFifty() {
+  }
+
+  function ownerWithdraw() owneronly public {
+    if (fees > 0) {
+      owner.call.value(fees)();
+    }
+  }
+
+  function ownerFees() owneronly public returns (uint) {
+    return fees;
+  }
+
+  function() pricecheck public {
+    seeda = (seeda * LEHMER_MUL) % LEHMER_MOD;
+    result = result ^ uint(sha3(block.coinbase, block.blockhash(block.number - 1), pool, seeda));
+
+    uint number = 0;
+
+    if (msg.value >= CONFIG_MAX_VALUE) {
+      number = CONFIG_MAX_PLAYS;
+    } else {
+      number = msg.value / CONFIG_PRICE;
+    }
+
+    uint input = number * CONFIG_PRICE;
+    uint output = 0;
+
+    for (uint num = 0; num < number; num++) {
+      seedb = (seedb * LEHMER_MUL) % LEHMER_MOD;
+      result = result ^ seedb;
+
+      if (result % 2 == 0) {
+        uint win = pool / 2;
+        uint fee = (win * CONFIG_FEES_MUL) / CONFIG_FEES_DIV;
+
+        pool -= win;
+        fees += fee;
+        output += win - fee + CONFIG_PRICE;
+      } else {
+        pool += CONFIG_PRICE;
+      }
+    }
+
+    // uint ratio = (output * CONFIG_RATIO_MUL) / input;
+    NewPlay(msg.sender, uint32(now), input, output, pool);
+
+    output += msg.value - input;
+    txs += number;
+
+    if (output > 0) {
+      msg.sender.call.value(output)();
+    }
+  }
+}
