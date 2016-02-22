c6291a25d7237b603dcdf9150bad492f53086d00
Xavier Yvez <xavieryvez@gmail.com>
2016-02-22 15:40:01 +0200

    simplify, range sum via input number

--- src/dice/dice.sol
+++ src/dice/dice.sol
@@ -14,33 +14,6 @@ contract LooneyDice {
     _
   }
 
-  // for each play of play, chance = <odds>/36 occurences (return), test is the value to be tested
-  struct Test {
-    uint8 chance;
-    uint8 test;
-  }
-
-  // these are the values of the input plays (byte in)
-  uint constant private PLAY_EVEN_SUM = 0x0;
-  uint constant private PLAY_ODD_SUM = 0x1;
-  uint constant private PLAY_SUM_2 = 0x2;
-  uint constant private PLAY_SUM_3 = 0x3;
-  uint constant private PLAY_SUM_4 = 0x4;
-  uint constant private PLAY_SUM_5 = 0x5;
-  uint constant private PLAY_SUM_6 = 0x6;
-  uint constant private PLAY_SUM_7 = 0x7;
-  uint constant private PLAY_SUM_8 = 0x8;
-  uint constant private PLAY_SUM_9 = 0x9;
-  uint constant private PLAY_SUM_10 = 0x10;
-  uint constant private PLAY_SUM_11 = 0x11;
-  uint constant private PLAY_SUM_12 = 0x12;
-  uint constant private PLAY_MORE_7 = 0xa;
-  uint constant private PLAY_LESS_7 = 0xb;
-  uint constant private PLAY_SINGLE = 0xc;
-  uint constant private PLAY_DOUBLE = 0xd;
-  uint constant private PLAY_EQUAL = 0xe;
-  uint constant private PLAY_NOT_EQUAL = 0xf;
-
   // number of different combinations for 2 six-sided dice
   uint constant private MAX_ROLLS = 6 * 6;
 
@@ -71,8 +44,8 @@ contract LooneyDice {
   uint private dicea = 0;
   uint private diceb = 0;
 
-  // based on the play of play (Even, Odd, Seven, etc.) map to the applicable test with odds
-  Test[255] private tests;
+  // the chances that a specific sum would arrise - 0, 1 (even, odd) & 2-12 (sum)
+  uint[13] private chances = [18, 18, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
 
   // publically available contract information
   uint public funds = msg.value;
@@ -83,69 +56,43 @@ contract LooneyDice {
 
   // basic constructor, since the initial values are set, just do something for the test/play plays
   function LooneyDice() {
-    // even & odd
-    tests[PLAY_EVEN_SUM] = Test({ chance: 18, test: 0 });
-    tests[PLAY_ODD_SUM] = Test({ chance: 18, test: 1 });
-
-    // sums 2-12, chance peaks at 7, then decreases to max
-    tests[PLAY_SUM_2] = Test({ chance: 1, test: 2 });
-    tests[PLAY_SUM_3] = Test({ chance: 2, test: 3 });
-    tests[PLAY_SUM_4] = Test({ chance: 3, test: 4 });
-    tests[PLAY_SUM_5] = Test({ chance: 4, test: 5 });
-    tests[PLAY_SUM_6] = Test({ chance: 5, test: 6 });
-    tests[PLAY_SUM_7] = Test({ chance: 6, test: 7 });
-    tests[PLAY_SUM_8] = Test({ chance: 5, test: 8 });
-    tests[PLAY_SUM_9] = Test({ chance: 4, test: 9 });
-    tests[PLAY_SUM_10] = Test({ chance: 3, test: 10 });
-    tests[PLAY_SUM_11] = Test({ chance: 2, test: 11 });
-    tests[PLAY_SUM_12] = Test({ chance: 1, test: 12 });
-
-    // >7 & <7, both 15/36 chance
-    tests[PLAY_MORE_7] = Test({ chance: 15, test: 0 });
-    tests[PLAY_LESS_7] = Test({ chance: 15, test: 0 });
-
-    // two dice are equal or not equal
-    tests[PLAY_EQUAL] = Test({ chance: 6, test: 0 });
-    tests[PLAY_NOT_EQUAL] = Test({ chance: 30, test: 0 });
-
-    // single & double digits
-    tests[PLAY_DOUBLE] = Test({ chance: 6, test: 0 });
-    tests[PLAY_SINGLE] = Test({ chance: 30, test: 0 });
   }
 
   // calculates the winner based on inputs & test
-  function isWinner(uint play, Test test) constant private returns (bool) {
+  function testWinner(uint play) constant private returns (uint, bool) {
     // ok, this is the sum, quite useful for the next ones
     uint sum = dicea + diceb;
 
-    // number matching
-    if (test.test >= 2) {
-      return sum == test.test;
+    // equal/odd matching
+    if (play <= 1) {
+      return (chances[play], (sum % 2) == play);
     }
 
-    // dice are equal/not equal
-    else if (play == PLAY_EQUAL) {
-      return dicea == diceb;
-    } else if (play == PLAY_NOT_EQUAL) {
-      return dicea != diceb;
+    // single number matching
+    if (play <= 12) {
+      return (chances[play], sum == play);
     }
 
-    // greater-than/less-than
-    else if (play == PLAY_MORE_7) {
-      return sum > 7;
-    } else if (play == PLAY_LESS_7) {
-      return sum < 7;
+    // get the number range from the input - <max><min>
+    uint maxsum = play / 10;
+    uint minsum = play % 10;
+
+    // a > b, both >= 2 & <= 12
+    if (minsum < maxsum && minsum >= 2 && maxsum <= 12) {
+      // store the calculated chance
+      uint chance = 0;
+
+      // look through the options and add the chances
+      for (uint test = minsum; test <= maxsum; test++) {
+        chance += chances[test];
+      }
+
+      // return the success & chance
+      return (chance, sum >= minsum && sum <= maxsum);
     }
 
-    // double/single digit sum
-    else if (play == PLAY_DOUBLE) {
-      return sum >= 10;
-    } else if (play == PLAY_SINGLE) {
-      return sum < 10;
-    }
-
-    // odd & even
-    return (sum % 2) == test.test;
+    // I still haven't found what I'm looking for
+    throw;
   }
 
   function roll() private returns (uint) {
@@ -173,25 +120,24 @@ contract LooneyDice {
   }
 
   // distribute fees, grabbing from the market-makers, allocating wins/losses as applicable
-  function execute(uint8 play, uint input) private returns (uint) {
-    // setup the play/test we are executing
-    Test memory test = tests[play];
-
-    // invalid play, don't execute
-    if (test.chance == 0) {
-      throw;
-    }
-
+  function execute(uint play, uint input) private returns (uint) {
     // fire up the random generator & roll the dice
     randomize();
+
+    // store if a winner and overall chance of this play
+    bool winner = false;
+    uint chance = 0;
+
+    // get the winning result
+    (chance, winner) = testWinner(play);
 
     // the actual returns that we send back to the user
     uint result = 0;
 
     // winning or losing outcome here?
-    if (isWinner(play, test)) {
+    if (winner) {
       // odds used as in divisor, i.e. evens = 36/18 = 200%, however input also gets added, so adjust
-      uint output = ((input * MAX_ROLLS) / test.chance) - input;
+      uint output = ((input * MAX_ROLLS) / chance) - input;
 
       // failsafe for the case where the contract runs out of funds
       if (output > funds) {
@@ -223,14 +169,14 @@ contract LooneyDice {
     txs += 1;
 
     // notify the world of this outcome
-    notifyPlayer(play, input, result);
+    notifyPlayer(play, chance, input, result);
 
     // ok, this is now what we owe the player
     return result;
   }
 
   // the play interface, used in the cases where we want to send a different-than-equal play through
-  function enter(byte play) public {
+  function enter(uint8 play) public {
     // we need to comply with the actual minimum values to be allowed to play
     if (msg.value < CONFIG_MIN_VALUE) {
       throw;
@@ -245,7 +191,7 @@ contract LooneyDice {
     }
 
     // get the actual return value for the player
-    uint output = execute(uint8(play), input) + (msg.value - input);
+    uint output = execute(play, input) + (msg.value - input);
 
     // do we need to send the player some ether, do it
     if (output > 0) {
@@ -262,7 +208,7 @@ contract LooneyDice {
       return;
     }
 
-    enter(byte(PLAY_EVEN_SUM));
+    enter(0);
   }
 
   // allow the owner to withdraw his/her fees
@@ -284,10 +230,10 @@ contract LooneyDice {
   }
 
   // log events
-  event Player(address addr, uint32 at, uint8 play, uint8 dicea, uint8 diceb, uint input, uint output, uint wins, uint txs, uint turnover);
+  event Player(address addr, uint32 at, uint8 play, uint8 chance, uint8 dicea, uint8 diceb, uint input, uint output, uint wins, uint txs, uint turnover);
 
   // send the player event, i.e. somebody has played, this is what he/she/it did
-  function notifyPlayer(uint play, uint input, uint output) private {
-    Player(msg.sender, uint32(now), uint8(play), uint8(dicea), uint8(diceb), input, output, wins, txs, turnover);
+  function notifyPlayer(uint play, uint chance, uint input, uint output) private {
+    Player(msg.sender, uint32(now), uint8(play), uint8(chance), uint8(dicea), uint8(diceb), input, output, wins, txs, turnover);
   }
 }
